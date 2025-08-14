1545b5cb24fa080935864e87416eeec779fdca0c
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-08-14 18:10:37 +0200

    Heading component incremented

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -1,3 +1,9 @@
 export default function Heading() {
-  return <h1>Kasa</h1>;
+  return (
+    <>
+      <img src="./src/assets/Screen-desktop-none.png" alt="Kasa" />
+      <img src="./src/assets/Image-source-Kasa.png" alt="Kasa" />
+      <h1>Chez vous, partout et ailleurs</h1>
+    </>
+  );
 }
