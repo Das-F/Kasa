2b91d610ce82a40bb823c01e2d3dd9c774436fe5
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-08-15 19:33:50 +0200

    header component corrected

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -1,9 +1,10 @@
+import "./Heading.css";
 export default function Heading() {
   return (
-    <>
+    <header className="heading">
       <img src="./src/assets/Screen-desktop-none.png" alt="Kasa" />
       <img src="./src/assets/Image-source-Kasa.png" alt="Kasa" />
       <h1>Chez vous, partout et ailleurs</h1>
-    </>
+    </header>
   );
 }
