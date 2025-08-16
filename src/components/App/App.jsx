b3d6e8fc08c76045f5c452e21d103f80497e1bef
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-08-16 11:23:41 +0200

    organisation of elements

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Heading from "./Heading.jsx";
+import Heading from "../Heading.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
