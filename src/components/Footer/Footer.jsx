44ff4c3072f2b4b99c46dad924805ee871b1e977
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-08-18 17:03:44 +0200

    footer added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <img src="./src/assets/Logo-footer.png" alt="Kasa" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+}
