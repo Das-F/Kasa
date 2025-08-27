ae4fe57995abf01462d7e46dacddb6513e39668f
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-08-27 15:52:43 +0200

    card added on Homepage

---
+++ src/components/HomePage/Card.jsx
@@ -0,0 +1,11 @@
+import "./Card.css";
+
+function Card() {
+  return (
+    <div className="card">
+      <h2 className="card-title">Title</h2>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import "../styles/Homepage.css";
 import Header from "../components/Header/Header.jsx";
+import Card from "../components/HomePage/Card.jsx";
 
 function HomePage() {
   return (
@@ -8,6 +9,11 @@ function HomePage() {
       <div className="banner">
         <img src="/images/Image-home-Kasa.png" alt="Kasa Home" className="banner-image" />
         <h1 className="banner-title">Chez vous, partout et ailleurs</h1>
+      </div>
+      <div className="cards-list">
+        <Card />
+        <Card />
+        <Card />
       </div>
     </>
   );
