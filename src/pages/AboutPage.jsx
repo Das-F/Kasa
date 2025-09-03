89b41bc6b662eababfa493b57136920fd2a71003
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-09-03 15:52:18 +0200

    collapses of aboutpage finished

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,10 +8,18 @@ function AboutPage() {
         <div className="about-banner">
           <img src="/images/image-about-Kasa.png" alt="about Kasa" className="about-image" />
         </div>
-        <DropdownBox title="Fiabilité" />
-        <DropdownBox title="Respect" />
-        <DropdownBox title="Service" />
-        <DropdownBox title="Sécurité" />
+        <DropdownBox title="Fiabilité">
+          <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
+        </DropdownBox>
+        <DropdownBox title="Respect">
+          <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+        </DropdownBox>
+        <DropdownBox title="Service">
+          <p>La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.</p>
+        </DropdownBox>
+        <DropdownBox title="Sécurité">
+          <p>La sécurité est la priorité chez Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+        </DropdownBox>
       </div>
     </>
   );
