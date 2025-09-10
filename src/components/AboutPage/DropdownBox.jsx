fc742d9d9cd216559ee70cd0f0aa89bc4f8a253c
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-09-10 10:22:31 +0200

    aboutPage finished

--- src/components/AboutPage/DropdownBox.jsx
+++ src/components/AboutPage/DropdownBox.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./DropdownBox.css";
 import DropdownArrow from "./DropdownArrow.jsx";
 
-function DropdownBox(props, className) {
+function DropdownBox({ title, children, className }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -11,13 +11,13 @@ function DropdownBox(props, className) {
   return (
     <div className={`dropdown ${className}`}>
       <div className="dropdown-box" onClick={toggleDropdown}>
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
         <div className={`arrow-wrapper ${isOpen ? "rotated" : ""}`}>
           <DropdownArrow />
         </div>
       </div>
 
-      {isOpen && <div className="dropdown-content">{props.children}</div>}
+      {isOpen && <div className="dropdown-content">{children}</div>}
     </div>
   );
 }
